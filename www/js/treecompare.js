ef77d10d906198818d27cf94e0dfdb2a05d0edb7
Panu Artimo <panu.artimo@sib.swiss>
2016-12-20 10:42:06 +0100

    - added worker helper function - added missing nhx tags for the parser (TODO S= nad SO=...)

--- www/js/treecompare.js
+++ www/js/treecompare.js
@@ -74,6 +74,31 @@ TreeCompare = (function() {
         autoCollapse: null
     };
 
+        //Add a work helper function to the jQuery object
+    $.work = function(args) {
+        var def = $.Deferred(function(dfd) {
+            var worker;
+            if (window.Worker) {
+                //Construct the Web Worker
+                var worker = new Worker(args.file);
+                worker.onmessage = function(event) {
+                    //If the Worker reports success, resolve the Deferred
+                    dfd.resolve(event.data);
+                };
+                worker.onerror = function(event) {
+                    //If the Worker reports an error, reject the Deferred
+                    dfd.reject(event);
+                };
+                worker.postMessage(args.args); //Start the worker with supplied args
+            } else {
+                //Need to do something when the browser doesn't have Web Workers
+            }
+        });
+
+        //Return the promise object (an "immutable" Deferred object for consumers to use)
+        return def.promise();
+    };
+
     /*
      called externally to get the TreeCompare object
      */
@@ -259,6 +284,8 @@ TreeCompare = (function() {
 
         var tokens = s.split(/\s*(;|\(|\[|\]|\)|,|:)\s*/); //already splits the NHX format as well
 
+        var nhx_tags = ['B=', 'S=', 'T=', 'E=', 'O=', 'SO=', 'L=' , 'Sw=', 'CO='];
+
         function getIdxToken(tokenArray, queryToken){
             var posTokens = [];
             for (var i = 0; i < tokenArray.length; i++){
@@ -335,12 +362,26 @@ TreeCompare = (function() {
                 case '['://TODO: input NHX format
                     var x = new_tokens[i + 1];
                     if (x.indexOf("&&NHX")!=-1){ //if NHX format
+
                         var nhx_tokens = x.split(/:/);
-                        var tmp_idx = check_nhx_variable(nhx_tokens, "B=");
-                        if (tmp_idx!=-1){
-                            var branchSupport = nhx_tokens[tmp_idx].split("=");
-                            tree.branchSupport = branchSupport[1];
-                        }
+                        console.log('=========nhx_tokens');
+                        console.log(nhx_tokens);
+
+                        // TODO, how to differentiate SO and O for example
+                        jQuery.each( nhx_tokens, function( i, nhx_token) {
+
+                            console.log("checking: "+nhx_token);
+
+                            var tmp_idx = check_nhx_variable(nhx_tokens, nhx_token);
+                            if (tmp_idx!=-1){
+                                var branchSupport = nhx_tokens[tmp_idx].split("=");
+                                console.log("Found: "+branchSupport[1]);
+                                tree.branchSupport = branchSupport[1];
+                            }
+
+                        });
+
+
 
                     }else{
                         if (!(x===";" || x==="")){
