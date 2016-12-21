8b7c54b5f8127b085acd6e44a6f6b0d359c17e8f
Severine Duvaud <severine.duvaud@isb-sib.ch>
2016-12-21 14:11:39 +0100

    Added unit test to the project

---
+++ www/jasmine/spec/TreeCompare.js
@@ -0,0 +1,36 @@
+/**
+ * Created by sduvaud on 20/12/16.
+ */
+
+describe("TreeCompare", function() {
+
+    //This will be called before running each spec
+    beforeEach(function () {
+        TreeCompare().init({
+            scaleColor: "white"
+        });
+    });
+
+    describe("testing basic functionality in the TreeCompare module", function(){
+        it("addTree should return the right tree from input data", function() {
+
+            var newick = '(A:0.1,B:0.2,(C:0.3,D:0.4):0.5);';
+
+            var myTree = TreeCompare().addTree(newick, 'myTestTree');
+            expect(myTree.name).toEqual('myTestTree');
+
+            var root = myTree.root;
+            expect(root.ID).toContain('node_');
+            expect(root.clickedParentHighlight).toBeFalsy();
+            expect(root.mouseoverHighlight).toBeFalsy();
+            expect(root.mouseoverLinkHighlight).toBeFalsy();
+            expect(root.correspondingHighlight).toBeFalsy();
+            expect(root.collapsed).toBeFalsy();
+
+            var first = root.children[0];
+            expect(root.children.length).toEqual(3);
+            expect(first.name).toEqual('A');
+            expect(first.length).toEqual(0.1);
+        });
+    });
+});
