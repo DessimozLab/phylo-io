21f3e6123e37d4dc361a94f405b27eaf78d2953e
Severine Duvaud <severine.duvaud@isb-sib.ch>
2017-01-11 15:38:24 +0100

    Added PNG as a download format. First draft version until further specifications

--- www/js/treecompare.js
+++ www/js/treecompare.js
@@ -2333,45 +2333,155 @@ var TreeCompare = function(){
      Update the content of the SVG download link
      */
     function updateDownloadLinkContent(canvasId) {
-        $("#downloadButtons" + canvasId).empty();
-        var html = d3.select("#" + canvasId + " svg")
-            .attr("version", 1.1)
-            .attr("xmlns", "http://www.w3.org/2000/svg")
-            .node().outerHTML;
-        var downloadButton = d3.select("#downloadButtons" + canvasId).append("a")
-            .attr("title", "file.svg")
-            .attr("href-lang", "image/svg+xml")
-            .attr("href", "data:image/svg+xml;base64,\n" + btoa(html))
-            .text("SVG")
-            .attr("class", "glyphicon glyphicon-download-alt");
 
-        $("#downloadButtons" + canvasId + " a").css({
-            "color": "#999"
+        $("#downloadButtons" + canvasId).empty();
+
+        // Code inspired by:
+        // https://bl.ocks.org/Rokotyan/0556f8facbaf344507cdc45dc3622177
+        var width = 300, height = 300;
+        var svg = d3.select("#" + canvasId + " svg")
+            .attr("version", 1.1)
+            .attr("xmlns", "http://www.w3.org/2000/svg");
+
+        var downloadButton = d3.select("#downloadButtons" + canvasId);
+
+        downloadButton.append("button")
+            .attr("id", "saveButton")
+            .attr("class", "btn btn-default dropdown-toggle")
+            .attr("id", "dropdownMenu1" + canvasId)
+            .attr("data-toggle", "dropdown")
+            .attr("aria-haspopup", "true")
+            .attr("aria-expanded", "true")
+            .text("Export ")
+            .append("span")
+            .attr("class", "caret");
+
+        downloadButton.append("ul")
+            .attr("class", "dropdown-menu")
+            .attr("aria-labelledby", "dropdownMenu1" + canvasId)
+            .attr("id", "exportList" + canvasId);
+
+        var item, text;
+        var svgString = getSVGString(svg.node());
+
+        item = document.createElement("li");
+        item.setAttribute("id", "savePng" + canvasId);
+        svgLink = document.createElement("a");
+        text = document.createTextNode("PNG");
+        svgLink.appendChild(text);
+        item.appendChild(svgLink);
+        document.getElementById("exportList" + canvasId).appendChild(item);
+
+        item = document.createElement("li");
+        item.setAttribute("id", "saveSvg" + canvasId);
+        svgLink = document.createElement("a");
+        svgLink.setAttribute("href", 'data:image/svg+xml;base64,' + btoa(svgString));
+        text = document.createTextNode("SVG");
+        svgLink.appendChild(text);
+        item.appendChild(svgLink);
+        document.getElementById("exportList" + canvasId).appendChild(item);
+
+        d3.select('#savePng' + canvasId).on('click', function () {
+            svgString2Image(svgString, 2 * width, 2 * height, 'png', save); // passes Blob and filesize String to the callback
+
+            function save(dataBlob, filesize) {
+                saveAs(dataBlob, 'phylo.io.png'); // FileSaver.js function
+            }
         });
 
+        // Below are the function that handle actual exporting:
+        // getSVGString (svgNode ) and svgString2Image( svgString, width, height, format, callback )
+        function getSVGString( svgNode ) {
+            svgNode.setAttribute('xlink', 'http://www.w3.org/1999/xlink');
+            var cssStyleText = getCSSStyles( svgNode );
+            appendCSS( cssStyleText, svgNode );
 
-        /*
-        Renaming download file to treename input box
-        Author: Jospeh Treford
-         */
-        isCompared = (document.getElementById('vis-container1').className.split(' ').indexOf('vis-container') === (-1));
-        if (isCompared) {
-            labelId = (canvasId[canvasId.length - 1] == 1) ? "newickIn1Label" : "newickIn2Label";
-        } else {
-            labelId = "newickInSingleLabel";
+            var serializer = new XMLSerializer();
+            var svgString = serializer.serializeToString(svgNode);
+            svgString = svgString.replace(/(\w+)?:?xlink=/g, 'xmlns:xlink=') // Fix root xlink without namespace
+            svgString = svgString.replace(/NS\d+:href/g, 'xlink:href') // Safari NS namespace fix
+
+            return svgString;
+
+            function getCSSStyles( parentElement ) {
+                var selectorTextArr = [];
+
+                // Add Parent element Id and Classes to the list
+                selectorTextArr.push( '#'+parentElement.id );
+                for (var c = 0; c < parentElement.classList.length; c++)
+                    if ( !contains('.'+parentElement.classList[c], selectorTextArr) )
+                        selectorTextArr.push( '.'+parentElement.classList[c] );
+
+                // Add Children element Ids and Classes to the list
+                var nodes = parentElement.getElementsByTagName("*");
+                for (var i = 0; i < nodes.length; i++) {
+                    var id = nodes[i].id;
+                    if ( !contains('#'+id, selectorTextArr) )
+                        selectorTextArr.push( '#'+id );
+
+                    var classes = nodes[i].classList;
+                    for (var c = 0; c < classes.length; c++)
+                        if ( !contains('.'+classes[c], selectorTextArr) )
+                            selectorTextArr.push( '.'+classes[c] );
+                }
+
+                // Extract CSS Rules
+                var extractedCSSText = "";
+                for (var i = 0; i < document.styleSheets.length; i++) {
+                    var s = document.styleSheets[i];
+
+                    try {
+                        if(!s.cssRules) continue;
+                    } catch( e ) {
+                        if(e.name !== 'SecurityError') throw e; // for Firefox
+                        continue;
+                    }
+
+                    var cssRules = s.cssRules;
+                    for (var r = 0; r < cssRules.length; r++) {
+                        if ( contains( cssRules[r].selectorText, selectorTextArr ) )
+                            extractedCSSText += cssRules[r].cssText;
+                    }
+                }
+                return extractedCSSText
+
+                function contains(str,arr) {
+                    return arr.indexOf( str ) === -1 ? false : true;
+                }
+            }
+
+            function appendCSS( cssText, element ) {
+                var styleElement = document.createElement("style");
+                styleElement.setAttribute("type","text/css");
+                styleElement.innerHTML = cssText;
+                var refNode = element.hasChildNodes() ? element.children[0] : null;
+                element.insertBefore( styleElement, refNode );
+            }
         }
-        download_fn = document.getElementById(labelId).value;
-        download_fn = (download_fn === '') ? "PhyloIO_Tree" : download_fn;
-        downloadButton.attr("download", download_fn);
 
-        /*
-        console.log(document.getElementById("newickIn1Label").value);
-            downloadButton.attr("download", document.getElementById("newickIn1Label").value);
-        } else {
-            console.log(document.getElementById("newickIn2Label").value);
-            downloadButton.attr("download", document.getElementById("newickIn2Label").value);
-        };
-        */
+        function svgString2Image( svgString, width, height, format, callback ) {
+            var format = format ? format : 'png';
+
+            var imgsrc = 'data:image/svg+xml;base64,'+ btoa( unescape( encodeURIComponent( svgString ) ) ); // Convert SVG string to dataurl
+
+            var canvas = document.createElement("canvas");
+            var context = canvas.getContext("2d");
+
+            canvas.width = width;
+            canvas.height = height;
+
+            var image = new Image;
+            image.onload = function() {
+                context.clearRect ( 0, 0, width, height );
+                context.drawImage(image, 0, 0, width, height);
+
+                canvas.toBlob( function(blob) {
+                    var filesize = Math.round( blob.length/1024 ) + ' KB';
+                    if ( callback ) callback( blob, filesize );
+                });
+            };
+            image.src = imgsrc;
+        }
         // TODO so at the moment, it works if you hover over the tree to cause the update funciton to work otherwise it saves it as 'download';
         // TODO it also does not work for the single view tree, as it uses the name from the last tree
     }
