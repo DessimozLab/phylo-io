c52f5176125c033acaa06409bd6e62bb6aaa15a1
F4llis <clement.train@gmail.com>
2022-11-20 20:01:43 +0100

    loader fix for stop wrokers

--- src/interface.js
+++ src/interface.js
@@ -431,7 +431,7 @@ export default class Interface {
                 elem.parentNode.removeChild(elem);
             });
             container_object.viewer.set_data(container_object.models[container_object.current_model]);
-            this.api.stop_worker()
+            container_object.api.stop_worker()
             container_object.compute_topology_and_render_bounded_viewer()
             container_object.viewer.render(container_object.viewer.hierarchy);
             container_object.viewer.update_collapse_level(container_object.models[container_object.current_model].settings.collapse_level)
